3fe3e68ab59fccda4fbb8420c54c513da916f227
HasanAasim <hasan.asim14@gmail.com>
2024-12-06 20:34:00 +0500

    New Test

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,16 @@ import { clerkMiddleware } from '@clerk/nextjs/server';
 export default clerkMiddleware();
 
 export const config = {
+  // matcher: [
+  //   // Skip Next.js internals and all static files
+  //   '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+  //   // Always run for API routes
+  //   '/(api|trpc)(.*)',
+  // ],
+
   matcher: [
-    // Skip Next.js internals and all static files
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:html|css|js|json|jpg|jpeg|png|gif|svg|ttf|woff|woff2|ico|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
+  
 };
